5de39c0cff86ff473a43fb55a11eb8aa08f66c32
Janika Savela <janika_s@hotmail.com>
2024-04-05 17:57:59 +0300

    routet edistyy

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true })) //purkaa req bodyn näkyväksi
+
 app.listen(3000, () => {
     console.log("Serving on port 3000")
 })
@@ -24,8 +26,24 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makeplace', async (req, res) => {
-    const place = new Places({ title: 'My Backyard'})
-    await place.save()
-    res.send(place)
+app.get('/places', async (req, res) => {
+    const places = await Places.find({})
+    res.render('places/index', {places})
 })
+
+app.get('/places/new', (req, res) => {
+    res.render('places/new')
+})
+
+app.post('/places', async (req, res) => {
+   const place = new Places(req.body.place)
+   await place.save()
+   res.redirect(`/places/${place._id}`)
+})
+
+app.get('/places/:id', async (req, res) => {
+    const place = await Places.findById(req.params.id)
+    res.render('places/show', { place })
+})
+
+
