657505f6f42b73f2380ec4b837a0e23806279a6f
Janika Savela <janika_s@hotmail.com>
2024-04-24 15:02:23 +0300

    Virheen hallintaa lisätty

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose')
 const Places = require('./models/places')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
+const { placeSchema } = require('./schemas')
 
 mongoose.connect('mongodb://localhost:27017/places')
 
@@ -22,6 +25,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true })) //purkaa req bodyn näkyväksi
 app.use(methodOverride('_method'))
 
+const validatePlace = (req, res, next) => {
+    const { error } = placeSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map( el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
 app.listen(3000, () => {
     console.log("Serving on port 3000")
 })
@@ -30,41 +43,51 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/places', async (req, res) => {
+app.get('/places', catchAsync(async (req, res) => {
     const places = await Places.find({})
     res.render('places/index', {places})
-})
+}))
 
 app.get('/places/new', (req, res) => {
     res.render('places/new')
 })
 
-app.post('/places', async (req, res) => {
+app.post('/places', validatePlace, catchAsync(async (req, res, next) => {
+//    if(!req.body.place) throw new ExpressError('Invalid Place data', 400)
    const place = new Places(req.body.place)
    await place.save()
-   res.redirect(`/places/${place._id}`)
-})
+   res.redirect(`/places/${place._id}`) 
+}))
 
-app.get('/places/:id', async (req, res) => {
+app.get('/places/:id', catchAsync(async (req, res) => {
     const place = await Places.findById(req.params.id)
     res.render('places/show', { place })
-})
+}))
 
-app.get('/places/:id/edit', async (req, res) => {
+app.get('/places/:id/edit', catchAsync(async (req, res) => {
     const place = await Places.findById(req.params.id)
     res.render('places/edit', { place })
-} )
+} ))
 
-app.put('/places/:id', async (req, res) => {
+app.put('/places/:id', validatePlace, catchAsync(async (req, res) => {
     const { id } = req.params
     const place = await Places.findByIdAndUpdate(id, {...req.body.place})
     res.redirect(`/places/${place._id}`)
-})
+}))
 
-app.delete('/places/:id', async (req, res) => {
+app.delete('/places/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     await Places.findByIdAndDelete(id)
     res.redirect('/places')
-}) 
+})) 
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err
+    if (!err.message) err.message = 'Oh no, something went wrong!'
+    res.status(statusCode).render('error', { err })
+})
 
