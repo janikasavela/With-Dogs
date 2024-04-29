1da83ab937b2731bbd48218ab24b6c2977005613
Janika Savela <janika_s@hotmail.com>
2024-04-29 09:04:22 +0300

    router suojaukset toimii

--- routes/places.js
+++ routes/places.js
@@ -16,6 +16,16 @@ const validatePlace = (req, res, next) => {
     }
 }
 
+const isAuthor = async (req, res, next) => {
+    const { id } = req.params
+    const place = await Places.findById(id)
+    if (!place.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have a permission!')
+        return res.redirect(`/places/${id}`)
+    }
+    next()
+}
+
 router.get('/', catchAsync(async (req, res) => {
     const places = await Places.find({})
     res.render('places/index', {places})
@@ -28,13 +38,14 @@ router.get('/new', isLoggedIn, (req, res) => {
 router.post('/', isLoggedIn, validatePlace, catchAsync(async (req, res, next) => {
 //    if(!req.body.place) throw new ExpressError('Invalid Place data', 400)
    const place = new Places(req.body.place)
+   place.author = req.user._id
    await place.save()
    req.flash('success', 'Succesfully added a new place!')
    res.redirect(`/places/${place._id}`) 
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const place = await Places.findById(req.params.id).populate('reviews')
+    const place = await Places.findById(req.params.id).populate('reviews').populate('author')
     if(!place) {
         req.flash('error', 'Cannot find that place!')
         return res.redirect('/places')
@@ -42,7 +53,8 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render('places/show', { place })
 }))
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    const { id } = req.params
     const place = await Places.findById(req.params.id)
     if(!place) {
         req.flash('error', 'Cannot find that place!')
@@ -51,14 +63,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
     res.render('places/edit', { place })
 } ))
 
-router.put('/:id', validatePlace, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validatePlace, catchAsync(async (req, res) => {
     const { id } = req.params
     const place = await Places.findByIdAndUpdate(id, {...req.body.place})
     req.flash('success', 'Successfully updated!')
     res.redirect(`/places/${place._id}`)
 }))
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params
     await Places.findByIdAndDelete(id)
     req.flash('success', 'Succesfully deleted place')
