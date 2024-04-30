eb37d34c061ff28506f8252bca9bfef0a6455032
Janika Savela <janika_s@hotmail.com>
2024-04-30 21:54:12 +0300

    tähtiarvostelu sujuu

--- routes/places.js
+++ routes/places.js
@@ -4,19 +4,17 @@ const catchAsync = require('../utils/catchAsync')
 const { isLoggedIn, isAuthor, validatePlace } = require('../middleware')
 const place = require('../controllers/places') 
 
-
-router.get('/', catchAsync(place.index))
+router.route('/')
+     .get(catchAsync(place.index))
+     .post(isLoggedIn, validatePlace, catchAsync(place.createNew))
 
 router.get('/new', isLoggedIn, place.newForm)
 
-router.post('/', isLoggedIn, validatePlace, catchAsync(place.createNew))
-
-router.get('/:id', catchAsync(place.showPlace))
+router.route('/:id')
+     .get(catchAsync(place.showPlace))
+     .put(isLoggedIn, isAuthor, validatePlace, catchAsync(place.updatePlace))
+     .delete(isLoggedIn, isAuthor, catchAsync(place.deletePlace)) 
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(place.editForm))
-
-router.put('/:id', isLoggedIn, isAuthor, validatePlace, catchAsync(place.updatePlace))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(place.deletePlace)) 
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const passport = require('passport')
 const { storeReturnTo } = require('../middleware')
 const users = require('../controllers/auth')
 
-router.get('/register', users.renderRegister)
+router.route('/register')
+     .get(users.renderRegister)
+     .post(catchAsync(users.register))
 
-router.post('/register', catchAsync(users.register))
-
-router.get('/login', users.renderLogin)
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), users.login)
+router.route('/login')
+     .get(users.renderLogin)
+     .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), users.login)
 
 router.get('/logout', users.logout)
 
