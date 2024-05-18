62cd52d4ab17e1feeb36d6d8bdbcfd6ea82f9dec
Janika Savela <janika_s@hotmail.com>
2024-05-18 15:38:36 +0300

    toimii paikkojen hakeminen halutulla tavalla

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require ('./models/user')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
 
 const userRoutes = require('./routes/users')
 const placeRoutes = require('./routes/places') 
@@ -35,13 +37,17 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true })) //purkaa req bodyn näkyväksi
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
+app.use(helmet({contentSecurityPolicy: false}))
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
